8d9b602804017cc60d7f8895e9077597c6ba17cf
Joaquin <joaquin.azcarate@free-now.com>
2020-08-18 12:59:09 +0200

    Super complex translation

--- src/lib/translate.js
+++ src/lib/translate.js
@@ -1,12 +1,45 @@
+const dict = [
+  { frasal: "descansa seguro", spanish: "esta seguro" },
+  { frasal: "foo", spanish: "bar" },
+];
+
+function preserveCase(original, toReplace) {
+  if (original == original.toUpperCase()) {
+    // Shout
+    return toReplace.toUpperCase();
+  } else if (original.charAt(0) == original.charAt(0).toUpperCase()) {
+    // Title
+    return toReplace.toUpperCase()[0] + toReplace.slice(1);
+  } else {
+    return toReplace;
+  }
+}
+
+function pure(block) {
+  return [{ original: block }];
+}
+
+function translate_(block, word) {
+  const { original } = block;
+  const normalize = original.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  const i = normalize.indexOf(word.frasal);
+  if (i !== -1) {
+    const match = original.substr(i, word.frasal.length);
+    return [
+      { original: original.substr(0, i) },
+      { original: match, translated: preserveCase(match, word.spanish) },
+      { original: original.substr(i + word.frasal.length) },
+    ];
+  }
+
+  return [block];
+}
+
 export function translate(block) {
-  return block
-    .split(" ")
-    .map((x) => x + " ")
-    .map((txt) => {
-      if (txt === "is ") {
-        return { original: txt, translated: "foo" };
-      } else {
-        return { original: txt };
-      }
-    });
+  const ret = dict.reduce((accum, toTranslate) => {
+    return accum.flatMap((x) => translate_(x, toTranslate));
+  }, pure(block));
+
+  console.log({ ret });
+  return ret;
 }
